01537189bf7220f895bc357f7bdfdb12781fbd47
Eduardo Cardoso <ygarasab@gmail.com>
2024-04-16 19:00:49 -0300

    first commit

---
+++ script/jurisprudencias.js
@@ -0,0 +1,236 @@
+$(document).ready(function () {
+  $("#numero_processo").mask("0#");
+
+  carregaListaJurisprudencias();
+});
+
+function mostraPesquisaAvacada() {
+  $(".inative").toggle();
+  $("#acoes").toggleClass("mt-3");
+  $("#icone_seta").toggleClass("fa-arrow-down fa-arrow-up");
+}
+
+function mostraMaisAcordao() {
+  $("#acordao_descricao").toggleClass("expandir");
+  $("#mostra_menos_acordao").toggleClass(
+    "fa-square-caret-down fa-square-caret-up"
+  );
+}
+
+function copiaEmenta() {
+  mostraAlerta("As informações da ementa foram copiadas com sucesso.");
+}
+
+function copiaAcordao() {
+  mostraAlerta("As informações do acordão foram copiadas com sucesso.");
+}
+
+function copiaInformacoesDoProcesso() {
+  mostraAlerta("As informações do processo foram copiadas com sucesso.");
+}
+
+function imprimiInteroTeor() {
+  var opcoes = {
+    margin: 1,
+    filename: "interero teor easyjur.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  };
+
+  html2pdf()
+    .set(opcoes)
+    .from($("#div_intero_teor_impressao")[0])
+    .toPdf()
+    .get("pdf")
+    .then(function (pdf) {
+      window.open(pdf.output("bloburl"), "_blank");
+    });
+}
+
+function imprimiListagem() {
+  var opcoes = {
+    margin: 1,
+    filename: "relação jurisprudências easyjur.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  };
+
+  html2pdf()
+    .set(opcoes)
+    .from($("#div_listagem_impressao")[0])
+    .toPdf()
+    .get("pdf")
+    .then(function (pdf) {
+      window.open(pdf.output("bloburl"), "_blank");
+    });
+}
+
+function inicialisaCalendarioNosInputs(selector) {
+  $(selector).datepicker({
+    dateFormat: "dd/mm/yy",
+    dayNames: [
+      "Domingo",
+      "Segunda",
+      "Terça",
+      "Quarta",
+      "Quinta",
+      "Sexta",
+      "Sábado",
+    ],
+    dayNamesMin: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    monthNames: [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ],
+    monthNamesShort: [
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez",
+    ],
+    nextText: "Próximo",
+    prevText: "Anterior",
+    changeYear: true,
+    yearRange: "c-100:c+20",
+  });
+}
+
+$(function () {
+  inicialisaCalendarioNosInputs("#data_inicio_publicacao");
+  inicialisaCalendarioNosInputs("#data_fim_publicacao");
+  inicialisaCalendarioNosInputs("#data_inicio_julgamento");
+  inicialisaCalendarioNosInputs("#data_fim_julgamento");
+});
+
+function mostraAlerta(content) {
+  $.alert({
+    title: '<img src="assets/img/logo.png" style="width: 135px">',
+    content: content,
+    buttons: {
+      alert: {
+        text: "OK",
+        btnClass: "btn-blue",
+      },
+    },
+  });
+}
+
+function removeBlankAttributes(obj) {
+    const result = {};
+    for (const key in obj) {
+        if (obj[key] !== null && obj[key] !== undefined && obj[key] !== "") {
+            result[key] = obj[key];
+        }
+    }
+    return result;
+}
+
+function carregaListaJurisprudencias(page=1) {
+
+  let dadosPesquisa = retornaDadosPesquisa();
+  dadosPesquisa = removeBlankAttributes(dadosPesquisa)
+  dadosPesquisa.page = page
+  let params = (new URLSearchParams(dadosPesquisa)).toString()
+  console.log(params);
+  $.ajax({
+    url: "http://localhost:7071/api/search?"+params,
+    method: "GET",
+    dataType: "json",
+    beforeSend: function (xhr) {
+      //mostrarLoader("Carregando lista de pop-ups cadastrados, por favor aguarde!");
+    },
+    success: function (response, textStatus, xhr) {
+      console.log(response.results);
+      let template_lista = retornaTemplateJurisprudeciaLista(response.results);
+      $("#corpo_listagem_tabela").html(template_lista);
+
+      let template_paginacao = retornaTemplatePaginacao(response.meta.page);
+      $("#footer_paginacao_pop-up").html(template_paginacao);
+
+      $("html, body").animate(
+        {
+          scrollTop: 0,
+        },
+        500
+      );
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      //alert_erro_popup("Desculpe, ocorreu um erro ao carregar a listagem. Por favor, atualize a página e tente novamente.");
+    },
+    complete: function (xhr, textStatus) {
+      // fecharLoader();
+    },
+  });
+}
+
+function pesquisarJurisprudencia(e, page = 1) {
+  e.preventDefault()
+  let dadosPesquisa = retornaDadosPesquisa();
+  dadosPesquisa.page = page
+
+  carregaListaJurisprudencias(dadosPesquisa);
+}
+
+function retornaDadosPesquisa() {
+  return {
+    q: $("#nome_ementa_acordao").val(),
+    with: $("#contem_palavra").val(),
+    without: $("#nao_contem_palavra").val(),
+    numeroProcesso: $("#numero_processo").val(),
+    orgaoJudicante: $("#orgao_judicante").val(),
+    relator: $("#ministo_relator").val(),
+    data_pub_inicio: $("#data_inicio_publicacao").val(),
+    data_pub_fim: $("#data_fim_publicacao").val(),
+    data_julgamento_inicio: $("#data_inicio_julgamento").val(),
+    data_julgamento_fim: $("#data_fim_julgamento").val(),
+    classeProcessual: $("#classe_processo").val(),
+    classeProcessual: $("#classe_processo").val(),
+    ordemListagem: $("#ordenar_listagem").val(),
+
+    checkboxSTJ: $('#checkbox_stj').is(':checked') ? 1 : 0,
+    checkboxTST: $('#checkbox_tst').is(':checked') ? 1 : 0,
+  };
+
+}
+
+
+function limparDadosPesquisa() {
+  $("#nome_ementa_acordao").val('');
+  $("#contem_palavra").val('');
+  $("#nao_contem_palavra").val('');
+  $("#numero_processo").val('');
+  $("#orgao_judicante").val('');
+  $("#ministo_relator").val('');
+  $("#data_inicio_publicacao").val('');
+  $("#data_fim_publicacao").val('');
+  $("#data_inicio_julgamento").val('');
+  $("#data_fim_julgamento").val('');
+  $("#classe_processo").val('');
+  $("#classe_processo").val('');
+  $("#ordenar_listagem").val('');
+
+
+  $('#checkbox_stj').prop('checked', false);
+  $('#checkbox_tst').prop('checked', false);
+}
---
+++ script/template_juris_lista.js
@@ -0,0 +1,163 @@
+const template_jurisprudencia = (item) => `
+<tr>
+    <td class="d-flex flex-column">
+        <span class="dado-tabela">
+            <span id="processo" class="titulo">Processo:</span>
+            <span>${item.processo.raw}</span>
+        </span>
+
+        <span class="dado-tabela">
+            <span id="orgao" class="titulo">Órgão:</span>
+            <span>${item.orgao.raw}</span>
+        </span>
+
+        <span class="dado-tabela">
+            <span id="orgao_judicante" class="titulo">Órgão Judicante:</span>
+            <span>${item.orgao_judicante.raw}</span>
+        </span>
+
+        <span class="dado-tabela">
+            <span id="ministro_relator" class="titulo">Ministro Relator:</span>
+            <span>${item.relator.raw}</span>
+        </span>
+
+        <span class="dado-tabela">
+            <span id="data_pubicacao" class="titulo">Data Publicação:</span>
+            <span>${formatarDataISO(item.data_publicacao_fonte.raw)}</span>
+        </span>
+
+        <span class="dado-tabela">
+            <span id="data_fim_pubicacao" class="titulo">Data Final da Publicação:</span>
+            <span>28/03/2024</span>
+        </span>
+
+        <span class="dado-tabela">
+            <span id="dataj" class="titulo">Data do Julgamento:</span>
+            <span>${formatarDataISO(item.data_julgamento.raw)}</span>
+        </span>
+
+        <span class="dado-tabela">
+            <span id="dataj" class="titulo">Data Final do Julgamento:</span>
+            <span>03/07/2024</span>
+        </span>
+
+        <a class="icon-link d-none" href="#"><i class="fa-solid fa-download "></i> Download da decisão
+        </a>
+    </td>
+
+    <td onclick="mostraPreviaEmenta('${item.id.raw}');">
+        <div data-id="${item.id.raw}" class="previa-ementa">${item.ementa.raw}</div>
+    </td>
+    
+    <td>
+        <div class="conteudo-botoes d-flex align-items-center justify-content-center w-100">
+            <div class="grid-acoes">
+                <a class="btn botao-primario" style="width: 3rem" title="Ver dados do processo" onclick="abrirUrlJurisprudencia('${item.id.raw}')"> 
+                    <i class="fa-solid fa-eye"></i>
+                </a>
+            </div>
+        </div>
+    </td>
+</tr>
+    `;
+
+function fazBotaoPaginacao(pagina)
+{
+    return `
+    <li class="page-item" id="page-btn-${pagina}"><a class="page-link">${pagina}</a></li>
+    `;
+}
+
+function montaPaginasAnteriores(atual)
+{
+    let pagina = atual - 1
+    let paginas = []
+
+    while(pagina > 1 && paginas.length < 5)
+    {
+        paginas.unshift(fazBotaoPaginacao(pagina))
+        pagina--
+    }
+
+    return paginas
+}
+function montaPaginasPosteriores(atual, max)
+{
+    let pagina = atual + 1
+    let paginas = []
+
+    while(pagina <= max && paginas.length < 5)
+    {
+        paginas.push(fazBotaoPaginacao(pagina))
+        pagina++
+    }
+
+    return paginas
+}
+
+function retornaTemplatePaginacao(paginacao)
+{
+
+    let paginas = []
+    const atual = paginacao.current
+
+    paginas.push(fazBotaoPaginacao(1))
+    paginas.push(...montaPaginasAnteriores(atual))
+    paginas.push(fazBotaoPaginacao(atual))
+    paginas.push(...montaPaginasPosteriores(atual, paginacao.total_pages))
+
+    
+
+
+    $(".pagination-container").html(`
+        <li class="page-item">
+            <a class="page-link" id="page-prev">Anterior</a>
+        </li>
+        ${paginas.join(' ')}
+        <li class="page-item">
+            <a class="page-link" id="page-next">Proxímo</a>
+        </li>
+    `)
+
+
+    $("#page-btn-"+atual).addClass('active')
+
+    $(".page-item").click(e => {
+        const id = e.delegateTarget.id
+        const page = id.split('-btn-')[1]
+        carregaListaJurisprudencias(page)
+    })
+    $("#page-prev").click(e => {
+        if(atual == 1) return
+        carregaListaJurisprudencias(atual - 1)
+    })
+    $("#page-next").click(e => {
+        if(atual == paginacao.total_pages) return
+        carregaListaJurisprudencias(atual + 1)
+    })
+}
+
+function retornaTemplateJurisprudeciaLista(dados){
+    return dados.map(template_jurisprudencia);
+    
+}
+
+function formatarDataISO(dataISO) {
+    const data = new Date(dataISO);
+    return data.toLocaleDateString('pt-BR');
+}
+
+function mostraPreviaEmenta(id) {
+    $(`.previa-ementa[data-id='${id}']`).toggleClass("mostrar-tudo");
+}
+
+function retornaUrlJurisprudencia(id){
+    let baseUrl = 'https://easyjur.com/blog/juris/';
+    return (baseUrl + id);
+   
+}
+
+function abrirUrlJurisprudencia(id){
+    const url = retornaUrlJurisprudencia(id);
+    window.open(url, '_blank');
+}
