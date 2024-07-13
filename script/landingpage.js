8b1ac5b05681b79dc8b5bdff7a17f682fa65ae2f
erik <earaujo@easyjur.com>
2024-07-12 22:50:27 -0300

    criacao dos modais hubspot

--- script/landingpage.js
+++ script/landingpage.js
@@ -57,45 +57,6 @@ hbspt.forms.create({
   target: "#whatsApp-form"
 });
 
-tippy('#instagram', {
-  content: 'Clique aqui e acesse nosso Instagram agora mesmo.',
-  placement: 'bottom-end',
-  arrow: false,
-  animation: 'fade',
-  followCursor: 'true'
-});
-
-tippy('#whatsapp', {
-  content: 'Converse conosco imediatamente pelo WhatsApp. Clique aqui para iniciar o chat.',
-  placement: 'bottom-end',
-  arrow: false,
-  animation: 'fade',
-  followCursor: 'true'
-});
-
-tippy('#youtube', {
-  content: 'Clique aqui e acesse já nosso canal no Youtube conheça o EasyJur.',
-  placement: 'bottom-end',
-  arrow: false,
-  animation: 'fade',
-  followCursor: 'true'
-});
-
-tippy('#legalacademy', {
-  content: 'Clique aqui e acesse já a melhor plataforma de treinamento para advogados do Brasil.',
-  placement: 'bottom-end',
-  arrow: false,
-  animation: 'fade',
-  followCursor: 'true'
-});
-
-tippy('#blog_easyjur', {
-  content: 'Clique aqui e fique por dentro de tudo sobre o mundo do direito e seus desafios.',
-  placement: 'bottom-end',
-  arrow: false,
-  animation: 'fade',
-  followCursor: 'true'
-});
 
 tippy('#top3_1', {
   content: 'Selo TOP3 Melhor escritório pequeno porte. B2B AWARDS 2024',
