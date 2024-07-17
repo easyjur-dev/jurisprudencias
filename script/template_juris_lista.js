103dceb05fd00ea921f3bce8928980759281bff9
erik <earaujo@easyjur.com>
2024-07-17 15:01:41 -0300

    pagination return

--- script/template_juris_lista.js
+++ script/template_juris_lista.js
@@ -5,32 +5,40 @@ const template_jurisprudencia = (item) => `
             <span id="processo" class="titulo">Processo:</span>
             <span>${item.processo.raw}</span>
         </span>
+
         <span class="dado-tabela">
             <span id="orgao" class="titulo">Órgão:</span>
             <span>${item.orgao.raw}</span>
         </span>
+
         <span class="dado-tabela">
             <span id="orgao_judicante" class="titulo">Órgão Judicante:</span>
             <span>${item.orgao_judicante ? item.orgao_judicante.raw : item.orgado_judicante.raw}</span>
         </span>
+
         <span class="dado-tabela">
             <span id="ministro_relator" class="titulo">Ministro Relator:</span>
             <span>${item.relator.raw}</span>
         </span>
+
         <span class="dado-tabela">
             <span id="data_pubicacao" class="titulo">Data Publicação:</span>
             <span>${formatarDataISO(item.data_publicacao_fonte.raw)}</span>
         </span>
+
         <span class="dado-tabela">
             <span id="dataj" class="titulo">Data do Julgamento:</span>
             <span>${formatarDataISO(item.data_julgamento.raw)}</span>
         </span>
+
         <a class="icon-link d-none" href="#"><i class="fa-solid fa-download "></i> Download da decisão
         </a>
     </td>
+
     <td onclick="mostraPreviaEmenta('${item.id.raw}');">
         <div data-id="${item.id.raw}" class="previa-ementa">${item.ementa.raw}</div>
     </td>
+    
     <td>
         <div class="conteudo-botoes d-flex align-items-center justify-content-center w-100">
             <div class="grid-acoes">
@@ -41,86 +49,90 @@ const template_jurisprudencia = (item) => `
         </div>
     </td>
 </tr>
-`;
+    `;
 
-function fazBotaoPaginacao(pagina) {
+function fazBotaoPaginacao(pagina)
+{
     return `
-    <li class="page-item" id="page-btn-${pagina}"><a class="page-link" data-page="${pagina}">${pagina}</a></li>
+    <li class="page-item" id="page-btn-${pagina}"><a class="page-link">${pagina}</a></li>
     `;
 }
 
-function montaPaginasAnteriores(atual) {
-    let pagina = atual - 1;
-    let paginas = [];
+function montaPaginasAnteriores(atual)
+{
+    let pagina = atual - 1
+    let paginas = []
 
-    while (pagina > 1 && paginas.length < 5) {
-        paginas.unshift(fazBotaoPaginacao(pagina));
-        pagina--;
+    while(pagina > 1 && paginas.length < 5)
+    {
+        paginas.unshift(fazBotaoPaginacao(pagina))
+        pagina--
     }
 
-    return paginas;
+    return paginas
+}
+function montaPaginasPosteriores(atual, max)
+{
+    let pagina = atual + 1
+    let paginas = []
+
+    while(pagina <= max && paginas.length < 5)
+    {
+        paginas.push(fazBotaoPaginacao(pagina))
+        pagina++
+    }
+
+    return paginas
 }
 
-function montaPaginasPosteriores(atual, max) {
-    let pagina = atual + 1;
-    let paginas = [];
+function retornaTemplatePaginacao(paginacao)
+{
 
-    while (pagina <= max && paginas.length < 5) {
-        paginas.push(fazBotaoPaginacao(pagina));
-        pagina++;
+    let paginas = []
+    const atual = paginacao.current
+
+    paginas.push(fazBotaoPaginacao(1))
+    if(atual !== 1)
+    {
+        paginas.push(...montaPaginasAnteriores(atual))
+        paginas.push(fazBotaoPaginacao(atual))
     }
+    paginas.push(...montaPaginasPosteriores(atual, paginacao.total_pages))
 
-    return paginas;
-}
+    
 
-function retornaTemplatePaginacao(paginacao) {
-    let paginas = [];
-    const atual = paginacao.current;
-
-    paginas.push(fazBotaoPaginacao(1));
-    if (atual !== 1) {
-        paginas.push(...montaPaginasAnteriores(atual));
-        paginas.push(fazBotaoPaginacao(atual));
-    }
-    paginas.push(...montaPaginasPosteriores(atual, paginacao.total_pages));
 
     $(".pagination-container").html(`
         <li class="page-item">
             <a class="page-link" id="page-prev">Anterior</a>
         </li>
-        ${paginas.join('')}
+        ${paginas.join(' ')}
         <li class="page-item">
             <a class="page-link" id="page-next">Proxímo</a>
         </li>
-    `);
+    `)
 
-    $("#page-btn-" + atual).addClass('active');
 
-    $(".page-link").click(function (e) {
-        e.preventDefault();
-        const page = $(this).data('page');
-        if (page) {
-            carregaListaJurisprudencias(page);
-        }
-    });
+    $("#page-btn-"+atual).addClass('active')
 
-    $("#page-prev").click(function (e) {
-        e.preventDefault();
-        if (atual > 1) {
-            carregaListaJurisprudencias(atual - 1);
-        }
-    });
-
-    $("#page-next").click(function (e) {
-        e.preventDefault();
-        if (atual < paginacao.total_pages) {
-            carregaListaJurisprudencias(atual + 1);
-        }
-    });
+    $(".page-item").click(e => {
+        const id = e.delegateTarget.id
+        const page = id.split('-btn-')[1]
+        carregaListaJurisprudencias(page)
+    })
+    $("#page-prev").click(e => {
+        if(atual === 1) return
+        carregaListaJurisprudencias(atual - 1)
+    })
+    $("#page-next").click(e => {
+        if(atual === paginacao.total_pages) return
+        carregaListaJurisprudencias(atual + 1)
+    })
 }
 
-function retornaTemplateJurisprudeciaLista(dados) {
+function retornaTemplateJurisprudeciaLista(dados){
     return dados.map(template_jurisprudencia);
+    
 }
 
 function formatarDataISO(dataISO) {
@@ -132,16 +144,16 @@ function mostraPreviaEmenta(id) {
     $(`.previa-ementa[data-id='${id}']`).toggleClass("mostrar-tudo");
 }
 
-function retornaUrlJurisprudencia(id) {
+function retornaUrlJurisprudencia(id){
     let baseUrl = '/documento/';
+    console.log(id)
     return (baseUrl + id);
+   
 }
 
-function abrirUrlJurisprudencia(id) {
-    const splitted = id.split('|');
-    id = splitted[0] + '/' + splitted[1];
+function abrirUrlJurisprudencia(id){
+    const splitted = id.split('|')
+    id = splitted[0] + '/' + splitted[1]
     const url = retornaUrlJurisprudencia(id);
     window.open(url, '_blank');
 }
-
-
